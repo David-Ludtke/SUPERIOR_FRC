8bc0e8996cea0e1c73d6e636c8e431f296e005e9
david lutke <36012@superior.k12.wi.us>
2025-11-12 12:08:20 -0600

    Fix output statement to print array elements
    Updated output statement to print array values.

--- src/Pointer.cpp
+++ src/Pointer.cpp
@@ -9,6 +9,6 @@ int main() {
   int array[10];
   setarray(array, 10, 70);
   for (int i = 0; i < 10; i += 1) {
-    array[i]; //print 70 ten times, no need for assignment because the pointer modifys the memory dirrectly without assignment
+    std::cout<<array[i]<<' '; //print 70 ten times, no need for assignment because the pointer modifys the memory dirrectly without assignment
   }
 }
