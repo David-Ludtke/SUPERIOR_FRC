5fc8c6ec76111f1fb00bf35a75f37f6053c60d10
david lutke <36012@superior.k12.wi.us>
2025-11-12 12:07:49 -0600

    Implement setarray function for array manipulation
    Added a function to set values in an array using pointers.

--- src/Pointer.cpp
+++ src/Pointer.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-int main() {
-  int var = 10;
-  int* pvar = &var;
-  for (int i  = 0; i < 100; i += 1) {
-    *pvar += i;
+void setarray(int* array, int length, int num) {
+  for (int i = 0; i < length; i += 1) {
+    array[i]; /*sugar for *(array + i)*/ = num; //unsafe because no way to validate the arrays length or to validate that array points to valid memory
   }
-  std::cout<<var<<'\n';
+}
+
+int main() {
+  int array[10];
+  setarray(array, 10, 70);
+  for (int i = 0; i < 10; i += 1) {
+    array[i]; //print 70 ten times, no need for assignment because the pointer modifys the memory dirrectly without assignment
+  }
 }
