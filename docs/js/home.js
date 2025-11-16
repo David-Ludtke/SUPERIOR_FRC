bb414daab44e2568142d76b83c3973ccab7beba2
Thatscooterguy12 <davidludtie@gmail.com>
2025-11-16 14:58:34 -0600

    ya some stuff happened

--- docs/js/home.js
+++ docs/js/home.js
@@ -1,36 +1,7 @@
-const button1 = document.getElementById('Rules');
-button1.addEventListener('click', function() {
-    window.location.href = './rules.html';
-})
-button1.addEventListener('mouseover', function() {
-    button1.style.backgroundColor = 'rgb(66, 66, 66)';
-    button1.style.color = 'rgb(151, 151, 151)';
-})
-button1.addEventListener('mouseout', function() {
-    button1.style.backgroundColor = 'grey';
-    button1.style.color = '#FBF7F5';
-})
-const button2 = document.getElementById('Docs');
-button2.addEventListener('click', function() {
-    window.location.href = './rules.html';
-})
-button2.addEventListener('mouseover', function() {
-    button2.style.backgroundColor = 'rgb(66, 66, 66)';
-    button2.style.color = 'rgb(151, 151, 151)';
-})
-button2.addEventListener('mouseout', function() {
-    button2.style.backgroundColor = 'grey';
-    button2.style.color = '#FBF7F5';
-})
-const button3 = document.getElementById('HowTo');
-button3.addEventListener('click', function() {
-    window.location.href = './rules.html';
-})
-button3.addEventListener('mouseover', function() {
-    button3.style.backgroundColor = 'rgb(66, 66, 66)';
-    button3.style.color = 'rgb(151, 151, 151)';
-})
-button3.addEventListener('mouseout', function() {
-    button3.style.backgroundColor = 'grey';
-    button3.style.color = '#FBF7F5';
-})
+const links = ['rules.html', './sfrc/sfrc.html', './howto/home.html'];
+const buttons = document.getElementsByClassName('nav-button');
+Array.from(buttons).forEach((button, index) => {
+    button.addEventListener('click', function() {
+        window.location.href = links[index];
+    });
+});
