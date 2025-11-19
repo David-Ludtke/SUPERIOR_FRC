997aa0e4fc42335c3c6415af5cb6a5f4fd228f6c
david lutke <36012@superior.k12.wi.us>
2025-11-19 14:31:57 -0600

    I changed some stuff

--- docs/js/include1.js
+++ docs/js/include1.js
@@ -1,19 +1,21 @@
 let path = document.currentScript.getAttribute('data-path'); 
 const ammendto = document.getElementsByTagName('head')[0];
+const bodyammendto = document.getElementsByTagName('body')[0];
 path = path.endsWith('/') ? path : path + '/';
-
 const includes = [
     'anchor.css',
     'body.css',
     'button.css',
+    'codeblocks.css',
     'header.css',
     'list.css',
-    'paragraph.css',
+    'paragragh.css',
     'whatis.css',
 ];
-let include = ['<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"></link>']; 
+let include = ['<link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">']; 
 
 includes.forEach(function(item) {
     include.push('<link rel="stylesheet" href="' + path + 'css/' + item + '">');
 });
 ammendto.innerHTML += include.join('\n');
+bodyammendto.innerHTML += '<button class="tight" style="margin-left: 5px;">Home</button>';
